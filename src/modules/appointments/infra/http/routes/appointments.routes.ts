2b46db42452c677e1a2bb115f956fbada4237972
André Machado <andrels.machado82@gmail.com>
2021-02-28 00:32:41 -0300

    Agenda do prestador

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,19 +1,17 @@
 import { Router } from 'express'
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController'
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 import ensureAuthenticated from '@modules/users/infra//http/middlewares/ensureAuthenticated'
 
 const appointmentsRouter = Router()
+
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated)
 
-// appointmentsRouter.get('/', async (req, res) => {
-//   const appointments = await appointmentsRepository.find()
-
-//   return res.json(appointments)
-// })
-
 appointmentsRouter.post('/', appointmentsController.create)
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter
